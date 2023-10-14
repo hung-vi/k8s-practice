f3e04fd71906f0b9ab1e156f10db4023b78941eb
hung-vi <ngochung.vi90@gmail.com>
2023-10-14 14:33:09 +0700

    Added K8s deployment for client

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@ docker build -t hungvi/k8s-practice-client -t hungvi/k8s-practice-client:$SHA -f
 
 docker push hungvi/k8s-practice-client:latest
 docker push hungvi/k8s-practice-client:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=hungvi/k8s-practice-client:$SHA

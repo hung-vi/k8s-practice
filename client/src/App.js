c32ecb3c7948f7ed902355055077a6cce6974bb9
hung-vi <ngochung.vi90@gmail.com>
2023-10-15 21:37:39 +0700

    Added docker-compose file for local development

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test travis CI auto build
+          Test local docker-compose
         </p>
         <a
           className="App-link"

11ebec24a3147fb554359ecb5536a27f62c24fc5
hung-vi <ngochung.vi90@gmail.com>
2023-10-25 23:21:40 +0700

    Changed client-deployment image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t hungvi/k8s-practice-client -t hungvi/k8s-practice-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hungvi/k8s-practice-client:latest -t hungvi/k8s-practice-client:$SHA -f ./client/Dockerfile ./client
 
 docker push hungvi/k8s-practice-client:latest
 docker push hungvi/k8s-practice-client:$SHA

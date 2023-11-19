5273f3af13545f355ca8b59c34d40b749f4680ac
hung-vi <ngochung.vi90@gmail.com>
2023-11-19 12:18:08 +0700

    Client to test domain

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test local docker-compose
+          Test domain vonga.online integration
         </p>
         <a
           className="App-link"
